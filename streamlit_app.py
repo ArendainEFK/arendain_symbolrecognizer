4f5c0db1164f64273cb0f6dd180dffd6d0be5d34
Arendain EFK <90166948+ArendainEFK@users.noreply.github.com>
2024-02-28 23:23:17 +0800

    Update streamlit_app.py
    Fix dataset info

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,11 +20,11 @@ def app():
     st.write('Dataset description:')
 
     st.write('Number of features: 64')
-    st.write(text)
     text = """Feature representation: Binary values (1 or 0) representing the 8x8 pixels of an image.
         The dataset consists of four groups with different variations as well as different
         shape classes. The dataset is for me is well varied and can be good for shape type
         classification or detail classifications."""
+        st.write(text)
 
     # display choice of classifier
     clf = BernoulliNB() 
